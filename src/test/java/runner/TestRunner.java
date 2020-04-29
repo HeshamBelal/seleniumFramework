64bace6309f242ee7439c8ca56b6fcdefc27dc4e
HeshamBelal <hesham.e.belal@gmail.com>
2020-04-30 02:24:47 +0300

    SauceLab Test addition

---
+++ src/test/java/runner/TestRunner.java
@@ -0,0 +1,9 @@
+package runner;
+
+import cucumber.api.CucumberOptions;
+import tests.TestBase;
+
+@CucumberOptions(features = "src/test/java/features/LoginBDD.feature", glue = {"src/test/java/steps"}, plugin = {"pretty","html:target/cucumber-html-report"})
+public class TestRunner extends TestBase {
+
+}
---
+++ src/test/java/steps/steps.java
@@ -0,0 +1,15 @@
+package steps;
+
+public class steps {
+    @cucumber.api.java.en.Given("The User press Sign in button")
+    public void theUserPressSignInButton() {
+    }
+
+    @cucumber.api.java.en.When("I entered valid email and password")
+    public void iEnteredValidEmailAndPassword() {
+    }
+
+    @cucumber.api.java.en.Then("The registration page is displayed successfully")
+    public void theRegistrationPageIsDisplayedSuccessfully() {
+    }
+}
